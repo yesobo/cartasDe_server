878bb39b593ebbec3f1de995cb639c147eaf7a57
yesobo <yeso40@hotmail.com>
2014-06-11 17:58:24 +0000

    change openshift port logic

--- server.js
+++ server.js
@@ -12,9 +12,15 @@ var bodyParser = require('body-parser');
 // this will let us get the data from a POST
 app.use(bodyParser());
 
-//var port = process.env.PORT || 3000; // codio settings
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000; 		// set our port
+var port = process.env.OPENSHIFT_NODEJS_PORT;
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+
+if (typeof port === "undefined") {
+    //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+    //  allows us to run/test the app locally.
+	port = "3000";
+    console.warn('No OPENSHIFT_NODEJS_PORT var, using ' + port);
+};
 if (typeof ipaddress === "undefined") {
     //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
     //  allows us to run/test the app locally.
