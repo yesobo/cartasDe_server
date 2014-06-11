90e816bd3185b9234dadd53a5d89b8e0d695aa22
yesobo <yeso40@hotmail.com>
2014-06-11 14:35:30 +0000

    ip address for openshift

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.use(bodyParser());
 // var port = process.env.PORT || 3000; // codio settings
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;; 		// set our port
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-if (typeof self.ipaddress === "undefined") {
+if (typeof ipaddress === "undefined") {
     //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
     //  allows us to run/test the app locally.
     console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-    self.ipaddress = "127.0.0.1";
+    ipaddress = "127.0.0.1";
 };
 
 
